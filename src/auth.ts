f0b099b8e02e480b4e9cecc6f0c7355ed4552e3e
Mohd Shuja Rizvi <mohdshuja2000@gmail.com>
2026-04-04 18:10:20 +0800

    Disable PKCE checks and use JWT session to fix custom domain login errors

--- src/auth.ts
+++ src/auth.ts
@@ -7,10 +7,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma) as any,
   secret: process.env.AUTH_SECRET,
   trustHost: true,
+  session: {
+    strategy: "jwt"
+  },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: ["none"], // Disabling PKCE checks as a temporary fix for Vercel/Custom Domain cookie sync issues
     })
   ],
   callbacks: {
